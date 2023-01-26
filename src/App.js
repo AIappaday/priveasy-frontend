18b12d85baa661df9becbd41fd80497396453fe5
sagardesai90 <saagardesai@gmail.com>
2023-01-25 21:13:30 -0800

    app updated from hackathon

--- src/App.js
+++ src/App.js
@@ -1,33 +1,141 @@
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
+import React, { Component } from "react";
 
-function App() {
-  const [question, setQuestion] = useState('');
+class App extends Component {
+  // const [question, setQuestion] = useState('');
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: null,
+      policyURL: null,
+      sarcasm: false,
+      loading: false
+    }
+    this.onQuestionSubmit = this.onQuestionSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.runQuery = this.runQuery.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
 
-  const onQuestionSubmit = function (e) {
-    console.log(question);
+  onQuestionSubmit = function (e) {
+    // console.log(question);
     e.preventDefaault();
     // Call API with question
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>PrivEasy</h1>
-        <form onSubmit={onQuestionSubmit}>
-          <label>
-            Type in Your Question:
+  onChange = e => {
+    // console.log("e: ", e);
+    this.setState({question: e.target.value});
+  }
+
+  onChangeURL = e => {
+    // console.log("e: ", e);
+    this.setState({policyURL: e.target.value});
+  }
+
+  handleOnChange = e => {
+    this.setState({sarcasm: e.target.checked});
+  }
+
+  async runQuery(e) {
+    e.preventDefault();
+    this.setState({ loading: true });
+    //eslint-disable-next-line
+    // const workflow = await fetch('https://prometheus-api.llm.llc/api/workflow/QqRqhvr5kRqFD2waSLUF',
+    // {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'accept': 'application/json',
+    //   },
+    //   body: {
+    //     'apiKey': "d3cab356-2ceb-4a22-9273-8f9f5857a2a4",
+    //     'args': [this.state.question],
+    //     'kwargs': {}
+    //   }
+    // })
+    if(this.state.sarcasm === true){
+      // eslint-disable-next-line
+      const workflow = await fetch("https://prometheus-api.llm.llc/api/workflow/DOJiRGoWyFQ3J8QJRNyl", {
+        body: JSON.stringify({'apiKey': '5b99ed99-462a-4a84-a234-aeb36935e39c', 'args': [ `${this.state.question}` ], 'kwargs': {
+        "URL" : `${this.state.policyURL}`,
+      }}),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }).then((response) => {
+        // console.log("response.json(): ", response.json());
+        return response.json();
+      }).then((data) => {
+        // let keys = Object.keys(data.executionTrace);
+        console.log("data: ", data.outputs[0])
+        // console.log("data: ", data.executionTrace[keys[2]].output, data.outputs[0]);
+        this.setState({output: data.outputs[0], loading: false});
+        return data;
+      });
+    } else {
+       // eslint-disable-next-line
+       const workflow = await fetch("https://prometheus-api.llm.llc/api/workflow/i4Sai2CZLOpepYLbhuUi", {
+        body: JSON.stringify({'apiKey': '5b99ed99-462a-4a84-a234-aeb36935e39c', 'args': [ `${this.state.question}` ], 'kwargs': {
+        "URL" : `${this.state.policyURL}`,
+      }}),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }).then((response) => {
+        // console.log("response.json(): ", response.json());
+        return response.json();
+      }).then((data) => {
+        // let keys = Object.keys(data.executionTrace);
+        console.log("data: ", data.outputs[0])
+        // console.log("data: ", data.executionTrace[keys[2]].output, data.outputs[0]);
+        this.setState({output: data.outputs[0], loading: false});
+        return data;
+      });
+    }
+    
+  }
+
+  render(){
+    const output = this.state.loading ? 'Loading...' : this.state.output;
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h4 className='title'>PrivEasy</h4>
+        </div>
+          <div className='card'>{output}</div>
+          <div className='form' onSubmit={this.onQuestionSubmit.bind(this)}>
+            <label>
+              <div className='question'>Type in Your Question:</div>
+             <div className='inputs'>
+              <br/>Policy URL: 
+                <input type="text"
+                      name="Policy URL" 
+                      value={this.state.policyURL}
+                      className="input"
+                      onChange={this.onChangeURL}/>
+                <br/> Question: 
+                <input type="text"
+                      name="question" 
+                      value={this.state.question}
+                      className="input"
+                      onChange={this.onChange}/>
+             </div>
+            </label>
             <br/>
-            <input type="text"
-                   name="question" 
-                   value={question}
-                   onChange={(e) => setQuestion(e.target.value)}/>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-  );
+            <div className='submit'>
+              <input type="checkbox" name="sarcasm" onClick={this.handleOnChange.bind(this)}/>
+              <p className='sarcasm'>(NSFW)</p>
+              <input type="submit" value="Submit" onClick={this.runQuery.bind(this)} />
+            </div>
+          </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
