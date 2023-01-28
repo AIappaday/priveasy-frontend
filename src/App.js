0abfc9f9b1dc335e77a55d1a54bb27bded2a6947
sagardesai90 <saagardesai@gmail.com>
2023-01-27 18:29:41 -0800

    add disclaimer

--- src/App.js
+++ src/App.js
@@ -106,32 +106,35 @@ class App extends Component {
         <div className="App-header">
           <h4 className='title'>PrivEasy</h4>
         </div>
-          <div className='card'>{output}</div>
-          <div className='form' onSubmit={this.onQuestionSubmit.bind(this)}>
-            <label>
-              <div className='question'>Type in Your Question:</div>
-             <div className='inputs'>
-              <br/>Policy URL: 
-                <input type="text"
-                      name="Policy URL" 
-                      value={this.state.policyURL}
-                      className="input"
-                      onChange={this.onChangeURL}/>
-                <br/> Question: 
-                <input type="text"
-                      name="question" 
-                      value={this.state.question}
-                      className="input"
-                      onChange={this.onChange}/>
-             </div>
-            </label>
-            <br/>
-            <div className='submit'>
-              <input type="checkbox" name="sarcasm" onClick={this.handleOnChange.bind(this)}/>
-              <p className='sarcasm'>(NSFW)</p>
-              <input type="submit" value="Submit" onClick={this.runQuery.bind(this)} />
+        <div className='card'>{output}</div>
+        <div className='form' onSubmit={this.onQuestionSubmit.bind(this)}>
+          <label>
+            <div className='question'>Type in Your Question:</div>
+            <div className='inputs'>
+            <br/>Policy URL: 
+              <input type="text"
+                    name="Policy URL" 
+                    value={this.state.policyURL}
+                    className="input"
+                    onChange={this.onChangeURL}/>
+              <br/> Question: 
+              <input type="text"
+                    name="question" 
+                    value={this.state.question}
+                    className="input"
+                    onChange={this.onChange}/>
             </div>
+          </label>
+          <br/>
+          <div className='submit'>
+            <input type="checkbox" name="sarcasm" onClick={this.handleOnChange.bind(this)}/>
+            <p className='sarcasm'>(NSFW)</p>
+            <input type="submit" value="Submit" onClick={this.runQuery.bind(this)} />
           </div>
+        </div>
+        <div className='disclaimer'>
+        The information provided by this app is for general informational purposes only. It is not intended to be legal advice or to be relied upon as a substitute for specific legal advice from a qualified attorney. The use of this app does not create an attorney-client relationship. The information provided in this app may not be accurate, complete, or up-to-date, and may not apply to the specific circumstances of your situation. You should not act or refrain from acting based on any information provided by this app without first seeking professional legal advice. The developers and authors of this app are not responsible for any errors or omissions in the information provided, or for any actions taken or not taken based on this information.
+        </div>
         
       </div>
     );
